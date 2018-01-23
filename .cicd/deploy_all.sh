70196267997320ff59083e9e89b125fb19b3a7b5
Leo Yu <leo@jwplayer.com>
2018-01-23 16:34:31 -0500

    fix deploy script

--- .cicd/deploy_all.sh
+++ .cicd/deploy_all.sh
@@ -8,17 +8,17 @@ function cicd_get_id {
 cicd pipeline update `cicd_get_id toy-celery-broker-backend` --service .cicd/broker-backend/service.yaml --stages .cicd/broker-backend/stages.yaml
 cicd pipeline deploy `cicd_get_id toy-celery-broker-backend`
 
-sed -i "s/latest/$1/g" .cicd/beat/service.yaml
+sed -i '' "s/latest/$1/g" .cicd/beat/service.yaml
 cicd pipeline update `cicd_get_id toy-celery-beat` --service .cicd/beat/service.yaml --stages .cicd/beat/stages.yaml
 cicd pipeline deploy `cicd_get_id toy-celery-beat`
 git checkout -- .cicd/beat/
 
-sed -i "s/latest/$1/g" .cicd/flower/service.yaml
+sed -i '' "s/latest/$1/g" .cicd/flower/service.yaml
 cicd pipeline update `cicd_get_id toy-celery-flower` --service .cicd/flower/service.yaml --stages .cicd/flower/stages.yaml
 cicd pipeline deploy `cicd_get_id toy-celery-flower`
 git checkout -- .cicd/flower/
 
-sed -i "s/latest/$1/g" .cicd/worker/service.yaml
+sed -i '' "s/latest/$1/g" .cicd/worker/service.yaml
 cicd pipeline update `cicd_get_id toy-celery-worker` --service .cicd/worker/service.yaml --stages .cicd/worker/stages.yaml
 cicd pipeline deploy `cicd_get_id toy-celery-worker`
 git checkout -- .cicd/worker/
